ed653a898c0df7ab27fbd1ea5b911dcaa8e3d862
az51911 <aleksandar.s.zivkovic@gmail.com>
2019-07-20 16:27:20 -0400

    minor readme change

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Currentvoteleader = ({ current_line }) => {
+    let current_leader_data = 0;
+    let current_leader_index = 0;
+
+    current_line.forEach(myFunction);
+    function myFunction(data, index) {
+        if (current_leader_data < data) {
+            current_leader_data = data;
+            current_leader_index = index;
+        }
+    }
+
+    return (
+        <div>
+            <h1>Anecdote with the most votes</h1>
+            <div>{anecdotes[current_leader_index]}</div>
+            has {current_leader_data} votes
+    </div>
+    );
+};
+
+const App = props => {
+    const [selected, setSelected] = useState(0);
+    const [vote_value, setVote] = useState(0);
+
+    const nextanecdote = () => {
+        let selected_val = Math.floor(Math.random() * 6);
+        setSelected(selected_val);
+    };
+
+    const handlevotes = () => {
+        vote_array[selected] = vote_array[selected] + 1;
+        setVote(vote_array[selected]);
+    };
+
+    const Currentvotenumber = () => {
+        return <div>has {vote_array[selected]} votes</div>;
+    };
+
+    return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            {props.anecdotes[selected]}
+            <Currentvotenumber current_line={vote_value} />
+            <div>
+                <Button onClick={handlevotes} text="vote" />
+                <Button onClick={nextanecdote} text="next anecdote" />
+            </div>
+            <Currentvoteleader current_line={vote_array} />
+        </div>
+    );
+};
+
+const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
+let vote_array = new Uint32Array(anecdotes.length);
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
--- part1/courseinfo/step2/readme.md
+++ part1/courseinfo/step2/readme.md
@@ -1,13 +1,2 @@
 1.2: course information, step2
 Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three Part components of which each renders the name and number of exercises of one part.
-
-const Content = ... {
-  return (
-    <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
-    </div>
-  )
-}
-Our application passes on information in quite a primitive way at the moment, since it is based on individual variables. This situation will improve soon.
